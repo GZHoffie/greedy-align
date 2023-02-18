9553767d6a1f99da6db8d9ad6f05ab18eee306e2
Gu Zhenhao <zhenhao.gu@u.nus.edu>
2023-02-18 16:00:25 +0800

    Debug for de Bruijn lanes implementation

--- aligner/greedy_aligner.hpp
+++ aligner/greedy_aligner.hpp
@@ -1,11 +1,13 @@
 #include "aligner.hpp"
 
 #include <seqan3/search/kmer_index/shape.hpp>
+#include <bitset>
 
 template<unsigned int READ_LENGTH>
 class de_bruijn_lanes {
 private:
     std::vector<std::bitset<READ_LENGTH>> lanes;
+    unsigned int bw;
 
 public:
     /**
@@ -13,7 +15,8 @@ public:
      * 
      * @param band_width Maximum number of indels allowed.
      */
-    de_bruijn_lanes(unsigned int band_width) {
+    de_bruijn_lanes(int band_width) {
+        bw = band_width;
         for (int i = -band_width; i <= band_width; i++) {
             std::bitset<READ_LENGTH> lane;
             lanes.push_back(lane);
@@ -36,7 +39,18 @@ public:
      * @return std::bitset<READ_LENGTH>& the reference to the bitset.
      */
     std::bitset<READ_LENGTH>& at(int i) {
-        return lanes[i + band_width];
+        return lanes[i + bw];
+    }
+
+    /**
+     * @brief Print out the lanes in order, for debugging.
+     * 
+     */
+    void print() {
+        seqan3::debug_stream << "[INFO]\t\t" << "Current de Bruijn lane layout:\n";
+        for (int i = 0; i < lanes.size(); i++) {
+            seqan3::debug_stream << "[INFO]\t\t" << "Lane " << i- (int)bw << "\t| " << lanes.at(i) << "\n";
+        }
     }
 };
 
@@ -55,25 +69,32 @@ public:
      */
     read_bit_vectors(const seqan3::dna4_vector& s) {
         for (int i = 0; i < s.size(); i++) {
-            b1[i] = s.to_rank() & 1;
-            b2[i] = s.to_rank() & 2;
+            b1[i] = s[i].to_rank() & 1;
+            b2[i] = s[i].to_rank() & 2;
         }
         length = s.size();
+        
     }
 
-    std::bitset<READ_LENGTH> shifted_xor(const read_bit_vectors& that, int shift_amount) {
+    std::bitset<READ_LENGTH> shifted_xor(const read_bit_vectors<READ_LENGTH>& that, int shift_amount) {
         std::bitset<READ_LENGTH> res;
         // void out all meaningless bits
         res = ~res << (READ_LENGTH - that.length);
+        //seqan3::debug_stream << shift_amount << " " << res << "\n";
+
         if (shift_amount > 0) {
             // void out the shifted bits
-            res = ~(res >> (READ_LENGTH - that.length) << shift_amount >> shift_amount);
+            res = ~(res >> (READ_LENGTH - that.length) >> shift_amount << shift_amount);
+            //seqan3::debug_stream << shift_amount << " " << res << "\n";
             // perform bitwise xor operation
             res |= ((b1 << shift_amount) ^ that.b1) | ((b2 << shift_amount) ^ that.b2);
+            //seqan3::debug_stream << shift_amount << " " << res << "\n";
         } else {
             // same as above, but in opposite direction
-            res = ~(res << shift_amount >> (shift_amount + READ_LENGTH - that.length));
+            res = ~(res << -shift_amount >> (-shift_amount + READ_LENGTH - that.length));
+            //seqan3::debug_stream << shift_amount << " " << res << "\n";
             res |= ((b1 >> -shift_amount) ^ that.b1) | ((b2 >> -shift_amount) ^ that.b2);
+            //seqan3::debug_stream << shift_amount << " " << res << "\n";
         }
         return res;
     }
@@ -84,7 +105,7 @@ template<unsigned int READ_LENGTH>
 class greedy_aligner : public aligner {
 private:
     unsigned int k;
-    unsigned int bw;
+    int bw;
     de_bruijn_lanes<READ_LENGTH>* lanes;
 
 
@@ -95,23 +116,34 @@ private:
      * @param v1, v2 the two sequences, already converted to bit vectors.
      * @param shift_amount The amount of shifting, which should be in [-band_width, band_width].
      */
-    void _shifted_xor(const read_bit_vectors& v1, const read_bit_vectors& v2, int shift_amount) {
+    void _shifted_xor(read_bit_vectors<READ_LENGTH>& v1, 
+                      read_bit_vectors<READ_LENGTH>& v2, 
+                      int shift_amount) {
         lanes->at(shift_amount) = v1.shifted_xor(v2, shift_amount);
+        
     }
 
+    /**
+     * @brief Find highways (common subpaths) in the de Bruijn graphs, which are represented by
+     *        (consecutive) zeros in the lanes.
+     * 
+     * @param s1, s2 the DNA sequences represented using seqan3::dna4 objects.
+     */
     void _build_de_bruijn_path(const seqan3::dna4_vector& s1, const seqan3::dna4_vector& s2) {
         // build read_bit_vectors objects base on the reads
-        read_bit_vectors v1(s1);
-        read_bit_vectors v2(s2);
+        read_bit_vectors<READ_LENGTH> v1(s1);
+        read_bit_vectors<READ_LENGTH> v2(s2);
 
         // perform shifting and bitwise xor operation, and store in `lanes`.
         for (int l = -bw; l <= bw; l++) {
             _shifted_xor(v1, v2, l);
-            for (int i = 0; i < k; i++) {
-                lanes->at(l) |= (lanes->at(l) << i);
+            auto original_lane = lanes->at(l);
+            for (int i = 1; i < k; i++) {
+                lanes->at(l) |= (original_lane >> i);
             }
         }
     }
+
 
 public:
     /**
@@ -123,14 +155,17 @@ public:
     greedy_aligner(unsigned int kmer_length, unsigned int band_width) : aligner() {
         k = kmer_length;
         bw = band_width;
+        lanes = new de_bruijn_lanes<READ_LENGTH>(bw);
+    }
+
+    ~greedy_aligner() {
+        delete lanes;
     }
 
     align_result_t align(seqan3::dna4_vector s1, seqan3::dna4_vector s2) {
         align_result_t res;
         _build_de_bruijn_path(s1, s2);
+        lanes->print();
         return res;
     }
-
-
-
 };
--- main.cpp
+++ main.cpp
@@ -1,5 +1,10 @@
 #include "aligner/greedy_aligner.hpp"
+#include "./utils.hpp"
 
 int main() {
+
+    using namespace seqan3::literals;
+    greedy_aligner<10> ga(3, 2);
+    ga.align("ACGTGCA"_dna4, "ACCTGCA"_dna4);
     return 0;
 }
